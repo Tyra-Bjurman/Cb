ec974571ca3ed7847601690edc126590d6aaa112
Tyra Bjurman <144486+Tyra-Bjurman@users.noreply.github.com>
2024-03-17 21:01:07 +0100

    Update Cb.h

--- Cb.h
+++ Cb.h
@@ -1,7 +1,7 @@
 // Variables
-#define u8   const unsigned char 
-#define u16  const unsigned short
-#define u32  const unsigned int
-#define s8   const signed char
-#define s16  const signed short
-#define s32  const signed int
+#define u8   (const unsigned char)
+#define u16  (const unsigned short)
+#define u32  (const unsigned int)
+#define s8   (const signed char)
+#define s16  (const signed short)
+#define s32  (const signed int)
